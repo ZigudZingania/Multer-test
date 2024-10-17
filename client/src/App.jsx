cf89926c6d94006a9dc64164fba48fa2af36c12b
Rohan <rohangorla07@gmail.com>
2024-10-17 15:21:19 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useRef } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [pathName, setPathName] = useState("");
+  const fileInputRef = useRef(null);
+
+  async function handleSubmit() {
+    const files = fileInputRef.current.files;
+    if (files.length > 0) {
+      let formData = new FormData();
+      for (let i = 0; i < files.length; i++) {
+        formData.append("files", files[i]);
+      }
+      const response = await axios.post(
+        "http://localhost:3000/send",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            name: `${pathName}-${Date.now()}`,
+          },
+        }
+      );
+      console.log(response);
+    }
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => {
+          setPathName(e.target.value);
+        }}
+      ></input>
+      <input type="file" accept="image/*" ref={fileInputRef} multiple></input>
+      <button onClick={handleSubmit}>submit</button>
+    </>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+import path from "path";
+import multer from "multer";
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3000;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const name = req.headers.name;
+    console.log(name);
+    const path = `./uploads/${name}`;
+    if (fs.existsSync(path)) {
+      cb(null, path);
+    } else {
+      fs.mkdirSync(path);
+      cb(null, path);
+    }
+  },
+  filename: function (req, file, callback) {
+    const extension = file.originalname.split(".")[1];
+    const fileName = file.originalname.split(".")[0];
+    callback(null, `${fileName}-${Date.now()}.${extension}`);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.get("/", (req, res) => {
+  res.json("Listening");
+});
+
+app.post("/send", upload.array("files"), (req, res) => {
+  console.log(req.files);
+  console.log(req.body.name);
+});
+
+app.listen(PORT, () => {
+  console.log("listening at http://localhost:3000");
+});
